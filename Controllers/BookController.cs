e392b32e098e732089448fcd20accbbf9a08e98f
Enes Doğan <enesdoganlive@hotmail.com>
2024-02-01 15:06:51 +0300

    Added directories

---
+++ Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Successful");
+        }
+    }
+}
---
+++ Models/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookWebApi.Models.Entities;
+
+public class Book : Entity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string AuthorName { get; set; }
+    public string CategoryName { get; set; }
+    public double Price { get; set; }
+    public int StockAmount { get; set; }
+}
